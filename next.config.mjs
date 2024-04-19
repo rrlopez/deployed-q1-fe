8c35854e1b6fed96439eb8c2311b0192a756a89d
Ricardo <you@example.com>
2024-04-19 15:26:06 +0800

    initial

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    env: {
+        ...Object.entries(process.env)
+          .filter(item => item[0].startsWith('APP_'))
+          .reduce((obj, item) => ({ ...obj, [item[0]]: item[1] }), {}),
+      },
+};
 
 export default nextConfig;
